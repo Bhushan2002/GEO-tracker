7731fd88d79ef43ca740efb5415d601a4ad8ef59
Bhushan <bwaghode321@gmail.com>
2025-12-20 20:02:05 +0530

    fixx

--- backend/dist/routes/modelResponse.route.js
+++ backend/dist/routes/modelResponse.route.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const modelRespons_controller_1 = require("../controllers/modelRespons.controller");
 const router = (0, express_1.Router)();
-router.get('/modelresponses', modelRespons_controller_1.getModelResponses);
+router.get('/', modelRespons_controller_1.getModelResponses);
 exports.default = router;
