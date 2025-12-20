d8c9a46fa016f80869de350bddf05f4e64de9f88
Bhushan <bwaghode321@gmail.com>
2025-12-20 19:11:50 +0530

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({origin:['http://localhost:3000']}))
+app.use(cors({origin:['http://localhost:3000' , 'geo-tracker-lpzspiu3q-bhushan-waghodes-projects.vercel.app']}))
 
 
 app.use("/api/prompt", promptRouter);
